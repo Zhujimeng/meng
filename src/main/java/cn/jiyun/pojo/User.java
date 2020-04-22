bc128049115000e770fdf687467b8ecd1a016346
Lenovo <2686269377@qq.com>
2020-04-22 12:42:11 +0800

    搭建meng

---
+++ src/main/java/cn/jiyun/pojo/User.java
@@ -0,0 +1,36 @@
+package cn.jiyun.pojo;
+
+/**
+ * @author shkstart
+ * @user 祝继蒙
+ * @date 2020/4/22 - 12:40
+ */
+public class User {
+    private String uid;
+    private String usex;
+    private String uaddress;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsex() {
+        return usex;
+    }
+
+    public void setUsex(String usex) {
+        this.usex = usex;
+    }
+
+    public String getUaddress() {
+        return uaddress;
+    }
+
+    public void setUaddress(String uaddress) {
+        this.uaddress = uaddress;
+    }
+}
